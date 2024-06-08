eea73b72de4305bb7fe53eb7cf64ab6d7b0cf5fb
psuarezdev <pablotablero@gmail.com>
2024-06-08 12:16:54 +0100

    reordering model attributes

--- elama/models.py
+++ elama/models.py
@@ -8,14 +8,14 @@ class Strategy(models.Model):
     title = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.title
+        return f'Strategy {self.id}'
 
 
 class Principle(models.Model):
+    strategy = models.ForeignKey(Strategy, on_delete=models.CASCADE)
     # This principle_id it's going to be used just for the order of the principles
     principle_id = models.IntegerField()
     title = models.CharField(max_length=255)
-    strategy = models.ForeignKey(Strategy, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Strategy {self.strategy.id} - Principle {self.principle_id}'
@@ -23,23 +23,24 @@ class Principle(models.Model):
 
 class Descriptor(models.Model):
     # This descriptor_id it's going to be used just for the order of the descriptors
+    strategy = models.ForeignKey(Strategy, on_delete=models.CASCADE)
+    principle = models.ForeignKey(Principle, on_delete=models.CASCADE)
     descriptor_id = models.IntegerField()
     title = models.CharField(max_length=255)
     content = models.TextField()
-    strategy = models.ForeignKey(Strategy, on_delete=models.CASCADE)
-    principle = models.ForeignKey(Principle, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return f'Strategy {self.strategy.id} - Principle {self.principle.principle_id} - Descriptor {self.descriptor_id}'
 
 
 class Individual(models.Model):
-    answer = models.IntegerField()
-    confirmed = models.BooleanField(default=False)
-    user_id = models.IntegerField()
     strategy = models.ForeignKey(Strategy, on_delete=models.CASCADE)
     principle = models.ForeignKey(Principle, on_delete=models.CASCADE)
     descriptor = models.ForeignKey(Descriptor, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
+    answer = models.IntegerField()
+    confirmed = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.strategy.id}.{self.principle.id}.{self.descriptor.id} - {self.answer}'
+        # TODO: Add the username
+        return f'Strategy {self.strategy.id} - Principle {self.principle.principle_id} - Descriptor {self.descriptor.descriptor_id} - Answer {self.answer}'
